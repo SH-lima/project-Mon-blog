8d5d6db408fd9d1eee23d5563c1f3fb7da6de838
SH-lima <halimasaid.pro@gmail.com>
2020-11-17 17:30:35 +0100

    Second commit

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
+// ----------menu burger 
+let  isFlipped = true
+// creer un fonction de deux parametres 
+  const afficherFenetre = (clickeBouton, fenetre) => {
+    //   mettre un ecouteur 
+    clickeBouton.addEventListener("click", ()=> {
+        // mettre une condition qui permettre d'afficher et fermer le menu
+        if(isFlipped){
+            // afficher le menu 
+            fenetre.classList.remove("afficheMenu")
+          isFlipped=false
+      }else{
+        //   fermer la fenetre
+        fenetre.classList.add("afficheMenu")
+          isFlipped=true
+      } 
+      })   
+  }
+
 const menu = document.querySelector(".modalMenu")
 const bouton = document.querySelector(".bouton")
-let  isFlipped = true
-bouton.addEventListener("click", ()=> {
-  if(isFlipped){
-     menu.classList.remove("afficheMenu")
-    isFlipped=false
-}else{
-    menu.classList.add("afficheMenu")
-    isFlipped=true
-} 
-})
 
 const iconDown = document.querySelector(".elementSousMenu")
 const sousMenu = document.querySelector(".afficheSousm")
-iconDown.addEventListener("click", ()=> {
-    if(isFlipped){
-       sousMenu.classList.remove("afficheMenu")
-      isFlipped=false
-  }else{
-      sousMenu.classList.add("afficheMenu")
-      isFlipped=true
-  } 
-  })
 
-
-  const iconDown2 = document.querySelector(".elementSousMenu2")
+const iconDown2 = document.querySelector(".elementSousMenu2")
 const sousMenu2 = document.querySelector(".afficheSousm2")
-iconDown2.addEventListener("click", ()=> {
-    if(isFlipped){
-       sousMenu2.classList.remove("afficheMenu")
-      isFlipped=false
-  }else{
-      sousMenu2.classList.add("afficheMenu")
-      isFlipped=true
-  } 
-  })
+// appeler la fonction afficherFenetre
+afficherFenetre(bouton, menu)
+afficherFenetre(iconDown, sousMenu)
+afficherFenetre(iconDown2,sousMenu2)
 
+
+// ----------modal le bienvenue 
+// selection l'element 
 const modal = document.querySelector("#main")
+// creer une fonction qui permet d'afficher le modal 
 const afficheModal = () =>{
+    // afficher la fenetre 
     modal.classList.remove('afficheMenu')
 }
-const boutonFermer = document.querySelector("#close-btn")
+// appeler la fonction afficheModal apres 1.5 s de l'ouverture de site 
 setTimeout(afficheModal, 1500)
+// selection le bouton qui permet de fermer le modal 
+const boutonFermer = document.querySelector("#close-btn")
+// mettre un ecouteur qui permet de fermer le modan en clickant sur le bouton 
 boutonFermer.addEventListener("click", ()=> {
     modal.classList.add('afficheMenu')
 });
